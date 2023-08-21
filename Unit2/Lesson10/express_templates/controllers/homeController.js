29b9dbcb675a2c4e0fb0cd070d2d8059fc152971
Lettie <lettiezulu764@gmail.com>
2023-08-21 18:20:42 +0200

    work complete

---
+++ Unit2/Lesson10/express_templates/controllers/homeController.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.sendReqParam = (req, res) => {
+  let veg = req.params.vegetable;
+  res.send(`This is the page for ${veg}`);
+};
+
+exports.respondWithName = (req, res) => { //added a function called respondWithName
+  res.render("index");  //we use a render method on the response object to respond with a view
+};
